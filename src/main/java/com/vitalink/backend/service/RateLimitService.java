8a00dd414fea170f2160b349378b1a10ae15145b
daniloza12 <87085703+daniloza12@users.noreply.github.com>
2026-03-05 21:40:00 -0500

    bd rateLimit

--- src/main/java/com/vitalink/backend/service/RateLimitService.java
+++ src/main/java/com/vitalink/backend/service/RateLimitService.java
@@ -39,28 +39,18 @@ public class RateLimitService {
     @Transactional
     public boolean isBlocked(HttpServletRequest request, String endpoint) {
         String ip = getClientIp(request);
-        LocalDateTime now = LocalDateTime.now();
 
-//        // 1. Verificar bloqueo activo
-//        List<RateLimit> blocks = rateLimitRepository.findActiveBlocks(ip, now);
-//        if (!blocks.isEmpty()) {
-//            log.warn("Blocked IP attempt - IP: {} blocked until: {}", ip, blocks.get(0).getBlockedUntil());
-//            return true;
-//        }
 
-        // 2. Registrar el hit
-        LocalDateTime blockedUntil = now.plusMinutes(blockMinutes);
-        rateLimitRepository.save(new RateLimit(null, ip, endpoint, blockedUntil));
-
-        // 3. Contar todos los hits de la IP en la ventana de tiempo (sin importar endpoint)
-//        LocalDateTime windowStart = now.minusMinutes(timeHitMinutes);
         long hitsInWindow = rateLimitRepository.countHitsInWindow(ip);
-
-        // 4. Bloquear si supera el límite
         if (hitsInWindow >= maxHits) {
             log.warn("IP blocked - IP: {} hits: {} ", ip, hitsInWindow);
             return true;
         }
+
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime blockedUntil = now.plusMinutes(blockMinutes);
+        rateLimitRepository.save(new RateLimit(null, ip, endpoint, blockedUntil));
 
         return false;
     }
